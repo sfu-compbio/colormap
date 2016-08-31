72f220343f56e8e7823c23e771f6c4ee5b18ebbf
Ehsan Haghshenas <ehaghshe@sfu.ca>
2016-08-30 20:18:07 -0700

    fix problem of run scripts when using absolute paths now it does not matter if the user passes absolute or relative paths

--- runCorr.sh
+++ runCorr.sh
@@ -1,29 +1,46 @@
-#!/bin/sh
+#!/bin/bash
 
 # Author: Ehsan Haghshenas (ehaghshe[at]sfu[dot]ca)
 
-if [ "$#" -lt 4 ]; then
-   echo "USAGE: ./runCorr.sh pacbio.fasta illumina.fastq outPrefix threads"
+if [ "$#" -lt 5 ]; then
+   echo "USAGE: ./runCorr.sh pacbio.fasta illumina.fastq outDirectory outPrefix threads"
    exit
 fi
 
-EXEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+EXEDIR=$(dirname "${BASH_SOURCE[0]}")
 CURRDIR=`pwd`
+LRFILE=$(realpath "${1}")
+SRFILE=$(realpath "${2}")
+OUTDIR=$(realpath "${3}")
+OUTPRE=$(basename "${4}")
+NTHREAD=$5
+BWAOPTIONS="-aY -A 5 -B 11 -O 2,1 -E 4,3 -k 8 -W 16 -w 40 -r 1 -D 0 -y 20 -L 30,30 -T 2.5 -t $NTHREAD"
 
 BWA=$EXEDIR/bin/bwa-proovread
 SAM=$EXEDIR/bin/samtools
 SPALG=$EXEDIR/bin/spCorrection
+OEAALG=$EXEDIR/bin/oeaCorrection
 
-mkdir -p $3
-rm -f $3/*
-ln -s -f $CURRDIR/$1 $3
+UNCORR="$OUTDIR/${OUTPRE}_uncorr"
+ITER1="$OUTDIR/${OUTPRE}_iter1"
+ITER2="$OUTDIR/${OUTPRE}_iter2"
 
-$BWA index $3/$1
-$BWA mem -aY -t $4 -A 5 -B 11 -O 2,1 -E 4,3 -k 8 -W 16 -w 40 -r 1 -D 0 -y 20 -L 30,30 -T 2.5 $3/$1 $2 > $3/$3_iter1.sam
-$SAM view -bS $3/$3_iter1.sam | $SAM sort -@ $4 - | $SAM view -h -o $3/$3_iter1.sort.sam -
-$SPALG -l $1 -a $3/$3_iter1.sort.sam -t $4 > $3/$3_iter1.fasta
+mkdir -p "$OUTDIR"
+# rm -f "$3"/*
+ln -s -f "$LRFILE" "${UNCORR}.fasta"
 
-$BWA index $3/$3_iter1.fasta
-$BWA mem -aY -t $4 -A 5 -B 11 -O 2,1 -E 4,3 -k 8 -W 16 -w 40 -r 1 -D 0 -y 20 -L 30,30 -T 2.5 $3/$3_iter1.fasta $2 > $3/$3_iter2.sam
-$SAM view -bS $3/$3_iter2.sam | $SAM sort -@ $4 - | $SAM view -h -o $3/$3_iter2.sort.sam -
-$SPALG -l $3/$3_iter1.fasta -a $3/$3_iter2.sort.sam -t $4 > $3/$3_corr.fasta
+"$BWA" index "${UNCORR}.fasta"
+"$BWA" mem $BWAOPTIONS "${UNCORR}.fasta" "$SRFILE" > "${ITER1}.sam"
+rm "${UNCORR}.fasta.amb" "${UNCORR}.fasta.ann" "${UNCORR}.fasta.bwt" "${UNCORR}.fasta.pac" "${UNCORR}.fasta.sa"
+"$SAM" view -bS "${ITER1}.sam" | "$SAM" sort -@ $NTHREAD - | "$SAM" view -h -o "${ITER1}.sort.sam" -
+rm "${ITER1}.sam"
+"$SPALG" -l "${UNCORR}.fasta" -a "${ITER1}.sort.sam" -t $NTHREAD > "${ITER1}.fasta"
+rm "${ITER1}.sort.sam"
+
+"$BWA" index "${ITER1}.fasta"
+"$BWA" mem $BWAOPTIONS "${ITER1}.fasta" "$SRFILE" > "${ITER2}.sam"
+rm "${ITER1}.fasta.amb" "${ITER1}.fasta.ann" "${ITER1}.fasta.bwt" "${ITER1}.fasta.pac" "${ITER1}.fasta.sa"
+"$SAM" view -bS "${ITER2}.sam" | "$SAM" sort -@ $NTHREAD - | "$SAM" view -h -o "${ITER2}.sort.sam" -
+rm "${ITER2}.sam"
+"$SPALG" -l "${ITER1}.fasta" -a "${ITER2}.sort.sam" -t $NTHREAD > "${ITER2}.fasta"
+rm "${ITER2}.sort.sam"
--- runOEA.sh
+++ runOEA.sh
@@ -1,24 +1,38 @@
-#!/bin/sh
+#!/bin/bash
 
 # Author: Ehsan Haghshenas (ehaghshe[at]sfu[dot]ca)
 
-if [ "$#" -lt 4 ]; then
-   echo "USAGE: ./runOEA.sh pacbio.fasta illumina.fastq outPrefix threads"
+if [ "$#" -lt 5 ]; then
+   echo "USAGE: ./runOEA.sh pacbio.fasta illumina.fastq outDirectory outPrefix threads"
    exit
 fi
 
-EXEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+EXEDIR=$(dirname "${BASH_SOURCE[0]}")
 CURRDIR=`pwd`
+LRFILE=$(realpath "${1}")
+SRFILE=$(realpath "${2}")
+OUTDIR=$(realpath "${3}")
+OUTPRE=$(basename "${4}")
+NTHREAD=$5
+BWAOPTIONS="-aY -A 5 -B 11 -O 2,1 -E 4,3 -k 8 -W 16 -w 40 -r 1 -D 0 -y 20 -L 30,30 -T 2.5 -t $NTHREAD"
+# BWAOPTIONS="-aY -t $NTHREAD"
 
 BWA=$EXEDIR/bin/bwa-proovread
 SAM=$EXEDIR/bin/samtools
+SPALG=$EXEDIR/bin/spCorrection
 OEAALG=$EXEDIR/bin/oeaCorrection
 
-mkdir -p $3
-rm -f $3/*
-ln -s -f $CURRDIR/$1 $3
+CORR="$OUTDIR/${OUTPRE}_corr"
+OEA="$OUTDIR/${OUTPRE}_oea"
 
-$BWA index $3/$1
-$BWA mem -aY -t $4 -A 5 -B 11 -O 2,1 -E 4,3 -k 8 -W 16 -w 40 -r 1 -D 0 -y 20 -L 30,30 -T 2.5 $3/$1 $2 > $3/$3.sam
-$SAM view -bS $3/$3.sam | $SAM sort -@ $4 - | $SAM view -h -o $3/$3.sort.sam -
-$OEAALG -s $2 -l $1 -a $3/$3.sort.sam -t $4 > $3/$3_oea.fasta
+mkdir -p "$OUTDIR"
+# # rm -f $3/*
+ln -s -f "$LRFILE" "${CORR}.fasta"
+
+$BWA index "${CORR}.fasta"
+$BWA mem $BWAOPTIONS "${CORR}.fasta" "$SRFILE" > "${OEA}.sam"
+rm "${CORR}.fasta.amb" "${CORR}.fasta.ann" "${CORR}.fasta.bwt" "${CORR}.fasta.pac" "${CORR}.fasta.sa"
+$SAM view -bS "${OEA}.sam" | $SAM sort -@ $NTHREAD - | $SAM view -h -o "${OEA}.sort.sam" -
+rm "${OEA}.sam"
+$OEAALG -s "$SRFILE" -l "${CORR}.fasta" -a "${OEA}.sort.sam" -t $NTHREAD > "${OEA}.fasta"
+rm "${OEA}.sort.sam"
