404c28f78f518a0b426346a203db17fb36ae6876
Ehsan Haghshenas <ehaghshe@sfu.ca>
2016-11-30 07:07:08 -0800

    fixing empty output directory

--- runCorr.sh
+++ runCorr.sh
@@ -11,37 +11,37 @@ EXEDIR=$(dirname "${BASH_SOURCE[0]}")
 CURRDIR=`pwd`
 LRFILE=$(readlink -e "${1}")
 SRFILE=$(readlink -e "${2}")
-OUTDIR=$(readlink -e "${3}")
+OUTDIR=$(readlink -f "${3}")
 OUTPRE=$(basename "${4}")
 NTHREAD=$5
 BWAOPTIONS="-aY -A 5 -B 11 -O 2,1 -E 4,3 -k 8 -W 16 -w 40 -r 1 -D 0 -y 20 -L 30,30 -T 2.5 -t $NTHREAD"
 
-BWA=$EXEDIR/bin/bwa-proovread
-SAM=$EXEDIR/bin/samtools
-SPALG=$EXEDIR/bin/spCorrection
-OEAALG=$EXEDIR/bin/oeaCorrection
+BWA=${EXEDIR}/bin/bwa-proovread
+SAM=${EXEDIR}/bin/samtools
+SPALG=${EXEDIR}/bin/spCorrection
+OEAALG=${EXEDIR}/bin/oeaCorrection
 
-UNCORR="$OUTDIR/${OUTPRE}_uncorr"
-  CORR="$OUTDIR/${OUTPRE}_sp"
- ITER1="$OUTDIR/${OUTPRE}_iter1"
- ITER2="$OUTDIR/${OUTPRE}_iter2"
+UNCORR=${OUTDIR}/${OUTPRE}_uncorr
+CORR=${OUTDIR}/${OUTPRE}_sp
+ITER1=${OUTDIR}/${OUTPRE}_iter1
+ITER2=${OUTDIR}/${OUTPRE}_iter2
 
-mkdir -p "$OUTDIR"
-# rm -f "$3"/*
-ln -s -f "$LRFILE" "${UNCORR}.fasta"
+mkdir -p ${OUTDIR}
+# rm -f ${OUTDIR}/*
+ln -s -f ${LRFILE} ${UNCORR}.fasta
 
-"$BWA" index "${UNCORR}.fasta"
-"$BWA" mem $BWAOPTIONS "${UNCORR}.fasta" "$SRFILE" > "${ITER1}.sam"
-rm "${UNCORR}.fasta.amb" "${UNCORR}.fasta.ann" "${UNCORR}.fasta.bwt" "${UNCORR}.fasta.pac" "${UNCORR}.fasta.sa"
-"$SAM" view -bS "${ITER1}.sam" | "$SAM" sort -@ $NTHREAD - | "$SAM" view -h -o "${ITER1}.sort.sam" -
-rm "${ITER1}.sam"
-"$SPALG" -l "${UNCORR}.fasta" -a "${ITER1}.sort.sam" -t $NTHREAD > "${ITER1}.fasta"
-rm "${ITER1}.sort.sam"
+${BWA} index ${UNCORR}.fasta
+${BWA} mem ${BWAOPTIONS} ${UNCORR}.fasta ${SRFILE} > ${ITER1}.sam
+rm ${UNCORR}.fasta.amb ${UNCORR}.fasta.ann ${UNCORR}.fasta.bwt ${UNCORR}.fasta.pac ${UNCORR}.fasta.sa
+${SAM} view -bS ${ITER1}.sam | ${SAM} sort -@ ${NTHREAD} - | ${SAM} view -h -o ${ITER1}.sort.sam -
+rm ${ITER1}.sam
+${SPALG} -l ${UNCORR}.fasta -a ${ITER1}.sort.sam -t ${NTHREAD} > ${ITER1}.fasta
+rm ${ITER1}.sort.sam
 
-"$BWA" index "${ITER1}.fasta"
-"$BWA" mem $BWAOPTIONS "${ITER1}.fasta" "$SRFILE" > "${ITER2}.sam"
-rm "${ITER1}.fasta.amb" "${ITER1}.fasta.ann" "${ITER1}.fasta.bwt" "${ITER1}.fasta.pac" "${ITER1}.fasta.sa"
-"$SAM" view -bS "${ITER2}.sam" | "$SAM" sort -@ $NTHREAD - | "$SAM" view -h -o "${ITER2}.sort.sam" -
-rm "${ITER2}.sam"
-"$SPALG" -l "${ITER1}.fasta" -a "${ITER2}.sort.sam" -t $NTHREAD > "${CORR}.fasta"
-rm "${ITER2}.sort.sam"
+${BWA} index ${ITER1}.fasta
+${BWA} mem ${BWAOPTIONS} ${ITER1}.fasta ${SRFILE} > ${ITER2}.sam
+rm ${ITER1}.fasta.amb ${ITER1}.fasta.ann ${ITER1}.fasta.bwt ${ITER1}.fasta.pac ${ITER1}.fasta.sa
+${SAM} view -bS ${ITER2}.sam | ${SAM} sort -@ ${NTHREAD} - | ${SAM} view -h -o ${ITER2}.sort.sam -
+rm ${ITER2}.sam
+${SPALG} -l ${ITER1}.fasta -a ${ITER2}.sort.sam -t ${NTHREAD} > ${CORR}.fasta
+rm ${ITER2}.sort.sam
--- runOEA.sh
+++ runOEA.sh
@@ -11,28 +11,27 @@ EXEDIR=$(dirname "${BASH_SOURCE[0]}")
 CURRDIR=`pwd`
 LRFILE=$(readlink -e "${1}")
 SRFILE=$(readlink -e "${2}")
-OUTDIR=$(readlink -e "${3}")
+OUTDIR=$(readlink -f "${3}")
 OUTPRE=$(basename "${4}")
 NTHREAD=$5
 BWAOPTIONS="-aY -A 5 -B 11 -O 2,1 -E 4,3 -k 8 -W 16 -w 40 -r 1 -D 0 -y 20 -L 30,30 -T 2.5 -t $NTHREAD"
-# BWAOPTIONS="-aY -t $NTHREAD"
 
-BWA=$EXEDIR/bin/bwa-proovread
-SAM=$EXEDIR/bin/samtools
-SPALG=$EXEDIR/bin/spCorrection
-OEAALG=$EXEDIR/bin/oeaCorrection
+BWA=${EXEDIR}/bin/bwa-proovread
+SAM=${EXEDIR}/bin/samtools
+SPALG=${EXEDIR}/bin/spCorrection
+OEAALG=${EXEDIR}/bin/oeaCorrection
 
-CORR="$OUTDIR/${OUTPRE}_corr"
-OEA="$OUTDIR/${OUTPRE}_oea"
+CORR=${OUTDIR}/${OUTPRE}_corr
+OEA=${OUTDIR}/${OUTPRE}_oea
 
-mkdir -p "$OUTDIR"
-# # rm -f $3/*
-ln -s -f "$LRFILE" "${CORR}.fasta"
+mkdir -p ${OUTDIR}
+# rm -f ${OUTDIR}/*
+ln -s -f ${LRFILE} ${CORR}.fasta
 
-$BWA index "${CORR}.fasta"
-$BWA mem $BWAOPTIONS "${CORR}.fasta" "$SRFILE" > "${OEA}.sam"
-rm "${CORR}.fasta.amb" "${CORR}.fasta.ann" "${CORR}.fasta.bwt" "${CORR}.fasta.pac" "${CORR}.fasta.sa"
-$SAM view -bS "${OEA}.sam" | $SAM sort -@ $NTHREAD - | $SAM view -h -o "${OEA}.sort.sam" -
-rm "${OEA}.sam"
-$OEAALG -s "$SRFILE" -l "${CORR}.fasta" -a "${OEA}.sort.sam" -t $NTHREAD > "${OEA}.fasta"
-rm "${OEA}.sort.sam"
+${BWA} index ${CORR}.fasta
+${BWA} mem ${BWAOPTIONS} ${CORR}.fasta ${SRFILE} > ${OEA}.sam
+rm ${CORR}.fasta.amb ${CORR}.fasta.ann ${CORR}.fasta.bwt ${CORR}.fasta.pac ${CORR}.fasta.sa
+${SAM} view -bS ${OEA}.sam | ${SAM} sort -@ ${NTHREAD} - | ${SAM} view -h -o ${OEA}.sort.sam -
+rm ${OEA}.sam
+${OEAALG} -s ${SRFILE} -l ${CORR}.fasta -a ${OEA}.sort.sam -t ${NTHREAD} > ${OEA}.fasta
+rm ${OEA}.sort.sam
